d1b677628a4068fdebd12957342a9b0de79d45bb
midhunkollillath <108817703+midhunkollillath@users.noreply.github.com>
2024-12-13 09:05:55 +0530

    some styling fixes

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,21 +1,36 @@
 import React from 'react';
-import { Box, Drawer, List, ListItem, ListItemText, IconButton } from '@mui/material';
+import { Box, Drawer, List, ListItem, ListItemText, IconButton, Typography } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
-import ProductsIcon from '@mui/icons-material/ShoppingCart'; 
+import ProductsIcon from '@mui/icons-material/ShoppingCart';
 import CustomersIcon from '@mui/icons-material/People';
 
 const Sidebar = ({ mobileOpen, handleDrawerToggle }) => {
   const drawerContent = (
-    <List>
-      <ListItem button component="a" href="/products">
-        <ProductsIcon sx={{ mr: 2 }} /> 
-        <ListItemText primary="Products" />
-      </ListItem>
-      <ListItem button component="a" href="/customers">
-        <CustomersIcon sx={{ mr: 2 }} />
-        <ListItemText primary="Customers" />
-      </ListItem>
-    </List>
+    <Box sx={{ p: 2 }}>
+      <Typography
+        variant="h6"
+        component="div"
+        sx={{
+          textAlign: 'center',
+          mb: 2,
+          fontWeight: 'bold',
+          color: 'primary.main',
+        }}
+      >
+        E-Commerce Cart
+      </Typography>
+
+      <List>
+        <ListItem button component="a" href="/products">
+          <ProductsIcon sx={{ mr: 2 }} />
+          <ListItemText primary="Products" />
+        </ListItem>
+        <ListItem button component="a" href="/customers">
+          <CustomersIcon sx={{ mr: 2 }} />
+          <ListItemText primary="Customers" />
+        </ListItem>
+      </List>
+    </Box>
   );
 
   return (
@@ -42,7 +57,7 @@ const Sidebar = ({ mobileOpen, handleDrawerToggle }) => {
       >
         {drawerContent}
       </Drawer>
-      
+
       <Drawer
         variant="permanent"
         sx={{
